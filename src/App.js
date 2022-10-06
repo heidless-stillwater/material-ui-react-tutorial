691660c009139bb225b12058e1e45ff7e164cc9f
heidless <rob.lockhart@yahoo.co.uk>
2022-10-06 20:33:11 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,47 @@
+import React from 'react'
 import logo from './logo.svg';
 import './App.css';
+import ButtonGroup from '@material-ui/core/ButtonGroup'
+import Button from '@material-ui/core/Button'
+import Checkbox from '@material-ui/core/Checkbox'
+
+
+import SaveIcon from '@material-ui/icons/Save'
+import DeleteIcon from '@material-ui/icons/Delete'
+import MailIcon from '@material-ui/icons/Mail'
+
+
+function CheckboxExample() {
+  const[checked, setChecked] = React.useState(true)
+  return (
+    <div>
+      <Checkbox 
+        checked={checked}
+        onChange={(e)=>setChecked(e.target.checked)}  
+      />
+    </div>
+  )
+}
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-header" color="primary">
+        <CheckboxExample />
+        <ButtonGroup variant="contained" color="primary">
+          <Button
+            startIcon={<SaveIcon />}
+          >
+            Save
+          </Button>
+          <Button 
+            startIcon={<DeleteIcon />}
+          >
+            Discard
+          </Button>
+        </ButtonGroup>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
